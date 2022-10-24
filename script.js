47fa090d583fcc12f9b89b3ec74dddb70dc1084b
Dilya <example@.gmail.com>
2022-10-24 16:41:38 +0600

    first app

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Создать переменную num
+ */
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из просмотренных фильмов?', ""),
+      b = prompt('Насколько оцените его',""),
+      c = prompt('Один из просмотренных фильмов?', ""),
+      d = prompt('Насколько оцените его',"");
+
+personalMovieDB.movies [a] = b;
+personalMovieDB.movies [c] = d;
+
+console.log(personalMovieDB);
